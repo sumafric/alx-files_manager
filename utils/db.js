41a0a44c079268ffb6da52378170a85e94897f67
sumafric <abelsum1330@gmail.com>
2025-06-30 13:11:06 -0700

    fixing DB

--- utils/db.js
+++ utils/db.js
@@ -1,51 +1,35 @@
-// Importing the required module from 'mongodb' package
 import { MongoClient } from 'mongodb';
 
-// Defining default connection parameters
-const host = process.env.DB_HOST || 'localhost';
-const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${host}:${port}/`;
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${HOST}:${PORT}`;
 
-// Class def for managing MongoDB connection and operations
 class DBClient {
-  constructor() {
-    // Initialize db property as null
-    this.db = null;
-    // Connect to MongoDB server using MongoClient
-    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      if (error) console.log(error);
-      this.db = client.db(database);
-      this.db.createCollection('users');
-      this.db.createCollection('files');
+  constructor () {
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
     });
   }
 
-  // Method to check if the database connection is alive
-  // Return true if this.db is not null, indicating the connection is alive
-  isAlive() {
-    return !!this.db;
+  isAlive () {
+    return this.client.isConnected();
   }
 
-  // Method to retrieve the number of documents in the 'users' collection
-  async nbUsers() {
-    return this.db.collection('users').countDocuments();
+  async nbUsers () {
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
-  // Method to retrieve a user from the 'users' collection based on a query
-  async getUser(query) {
-    console.log('QUERY IN DB.JS', query);
-    const user = await this.db.collection('users').findOne(query);
-    console.log('GET USER IN DB.JS', user);
-    return user; // Return the retrieved user
-  }
-
-  // Method to retrieve the number of documents in the 'files' collection
-  async nbFiles() {
-    return this.db.collection('files').countDocuments();
+  async nbFiles () {
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
-// Instantiate DBClient class
-const dbClient = new DBClient();
 
-export default dbClient;
+const dbClient = new DBClient();
